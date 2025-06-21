53c6c07e673e83b03906026247ae67101b2eb92f
Raja Akash <rajaakashk9@gmail.com>
2025-06-21 10:46:29 +0530

    fixing merge conflit

--- src/components/thread/index.tsx
+++ src/components/thread/index.tsx
@@ -370,9 +370,11 @@ export function Thread() {
               </div>
             </div>
           )} */}
+
+          
           {chatStarted && (
             <div className="relative z-10 flex items-center justify-between gap-3 bg-white p-2">
-              <div className="relative flex items-center justify-start gap-2">
+              <div className="relative flex items-center justify-start gap-2">/
                 {/* <div className="absolute left-0 z-10">
                   {(!chatHistoryOpen || !isLargeScreen) && (
                     <Button
