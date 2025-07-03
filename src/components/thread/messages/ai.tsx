5b064ae56f3e2b757f7e2695e203a9b9ebce6ad9
Ritwik Kanodia <external@neocarbtechnology.com>
2025-07-03 20:03:37 +0800

    check message undefined

--- src/components/thread/messages/ai.tsx
+++ src/components/thread/messages/ai.tsx
@@ -159,7 +159,7 @@ export function AssistantMessage({
           </>
         ) : (
           <>
-            {message.id?.includes("search-offers-run") ? (
+            {message && message.id?.includes("search-offers-run") ? (
               <ExtractOfferFromMessages
                 messages={[message]}
                 onOfferSelect={onOfferSelect}
